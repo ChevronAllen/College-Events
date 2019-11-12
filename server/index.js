3d36c30cea0fda4e87c30f0001d556ec1edd68fc
Chevron Allen <chevronallen@yahoo.com>
2019-11-12 11:49:09 -0500

    Login & Registration API
    The skeleton of  these api functions are set up.

--- server/index.js
+++ server/index.js
@@ -6,8 +6,16 @@ var cookieParser = require('cookie-parser');
 var session = require('express-session');
 const numCPUs = require('os').cpus().length;
 
+const crytpo = require('crypto');
+const SALT_LENGTH = 16;
+const HASH_TYPE = 'sha256';
+const HASH_OUT = 'hex';
+const mysql = require('mysql');
+
 const isDev = process.env.NODE_ENV !== 'production';
 const PORT = process.env.PORT || 5000;
+
+
 
 // Multi-process to utilize all CPU cores.
 if (!isDev && cluster.isMaster) {
@@ -24,8 +32,21 @@ if (!isDev && cluster.isMaster) {
 
 } else {
   const app = express();
+  
+  //  Create Connection Object
+  var conn = mysql.createConnection({
+    host: "otmaa16c1i9nwrek.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+    user: "odexbq57g80uqove",
+    password: "zafxdf0tnbo7vebq",
+    database: "euwtker4demcwlxt",
+    port: "3306"
+  });
 
-
+  //  Attempt Connection to database
+  conn.connect(function(err){
+    if(err){ throw err; }
+    console.log("...Connected");
+  });
 
   // Priority serve any static files.
   app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
@@ -39,7 +60,45 @@ if (!isDev && cluster.isMaster) {
     res.send('{"message":"Hello from the custom server!"}');
   });
 
+  app.post('/registerAPI', function (req, res) {
+    var email = req.body['email'] ;
+    var hashAuth = req.body['auth'];
+    var salt = crytpo.randomBytes(SALT_LENGTH);
+
+    var hmac = crytpo.createHmac(HASH_TYPE,salt);
+    hmac.update(hashAuth);
+    
+
+    var sql = `CALL euwtker4demcwlxt.proc_user_register( '${email}', '${salt}', '${hmac.digest(HASH_OUT)}' )`;
+    //  TODO: Session ids can still avoid MitM attacks. The less times the front end sends authentication data, the better.
+
+    conn.query(sql, function(err,result){
+      //  Process query result
+      if(err){ throw err; }
+
+
+    });
+
+    res.send(req.body.email);
+  });
+
   app.post('/loginAPI', function (req, res) {
+    var email = req.body['email'] ;
+    var hashAuth = req.body['auth'];
+
+    //  TODO: Query to fetch salt (also checks if user is valid)
+
+    //  TODO: Query to Login (sort out what data the front end needs to be sent on a success)
+
+    var sql = "";
+
+    conn.query(sql, function(err,result){
+      //  Process query result
+      if(err){ throw err; }
+
+
+    });
+
     res.send(req.body.email);
   });
 
